bd225b9d10ab018c05deb6409897ad3146ccc264
Kalleb <kallebeliasmaestri1@gmail.com>
2023-11-29 00:53:35 -0300

    isr

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "342de811a4a35720"; }
+/******/ 		__webpack_require__.h = function() { return "8496e3e9b04c3d15"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
